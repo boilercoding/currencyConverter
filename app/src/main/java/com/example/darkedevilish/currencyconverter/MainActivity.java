5b64ae30008327b1918e3e8fcda961420d5c2882
boilercoding <boilercoding@gmail.com>
2017-08-12 17:31:22 -0400

    Adds converter

--- app/src/main/java/com/example/darkedevilish/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/darkedevilish/currencyconverter/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.darkedevilish.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view) {
+        EditText dollarAmountEditText = (EditText) findViewById(R.id.dollarAmountEditText);
+        Double dollarAmountDouble = Double.parseDouble(dollarAmountEditText.getText().toString());
+        Double rdPesoAmount = dollarAmountDouble * 47.27;
+
+        Toast.makeText(MainActivity.this, "RD$" + rdPesoAmount.toString(), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
